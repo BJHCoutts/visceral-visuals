5e931ea10c50561c92dbd0fc3d58ea4cdc1d737c
Brian Coutts <bjhcoutts@gmail.com>
2019-10-26 12:57:38 -0700

    removed contentful auto page generation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,27 @@
 const path = require("path")
 
-module.exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const productTemplate = path.resolve("./src/templates/blog-post.tsx")
-  const res = await graphql(`
-    query {
-      allContentfulProduct {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-    }
-  `)
+// module.exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const productTemplate = path.resolve("./src/templates/blog-post.tsx")
+//   const res = await graphql(`
+//     query {
+//       allContentfulProduct {
+//         edges {
+//           node {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  res.data.allContentfulProduct.edges.forEach(edge => {
-    createPage({
-      component: productTemplate,
-      path: `/${edge.node.slug}`,
-      context: {
-        slug: edge.node.slug,
-      },
-    })
-  })
-}
+//   res.data.allContentfulProduct.edges.forEach(edge => {
+//     createPage({
+//       component: productTemplate,
+//       path: `/${edge.node.slug}`,
+//       context: {
+//         slug: edge.node.slug,
+//       },
+//     })
+//   })
+// }
