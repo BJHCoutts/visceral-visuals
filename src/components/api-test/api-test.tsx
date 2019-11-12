dce09b185e5f4ee6dc692220dfb33a3e4170005e
Brian Coutts <bjhcoutts@gmail.com>
2019-11-11 16:36:43 -0800

    useEffect(()=>{},[]) properly implimented

--- src/components/api-test/api-test.tsx
+++ src/components/api-test/api-test.tsx
@@ -5,39 +5,42 @@ export const APITest = () => {
 
 	const [jsonplaceholder, setJsonPlaceholder] = React.useState("loading...")
 	const [dadJoke, setDadJoke] = React.useState("loading...")
-	const [apiCom, setApiCom] = React.useState("loading")
+	const [dailyFact, setDailyFact] = React.useState("loading")
 
-	const fetchJSONPlaceholder = async () => {
+	React.useEffect(() => {
+		const fetchJSONPlaceholder = (async () => {
 
-		const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-		if (res.status === 200) {
-			const data = await res.json()
-			return setJsonPlaceholder(data.title)
-		}else{
-			throw new Error('unable to fetch JSON Placeholder')
-		}
-	}
-
-	fetchJSONPlaceholder()
-
-React.useEffect(() => {	const fetchDadJoke = async () => {
-		const settings = {
-			headers: {
-				Accept: 'application/json'
+			const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+			if (res.status === 200) {
+				const data = await res.json()
+				return setJsonPlaceholder(data.title)
+			}else{
+				throw new Error('unable to fetch JSON Placeholder')
 			}
-		}
-		const res = await fetch('https://icanhazdadjoke.com', settings)
-		const data = await res.json()
-		return setDadJoke(data.joke)
-	}
-	
-	fetchDadJoke()})
-		
-	const fetchRandomFact = async () => {
-		const settings = {}
-		const res = await fetch(`https://api.fungenerators.com`)
+		})()
+	}, [])	
 
-	}
+	React.useEffect(() => {	const fetchDadJoke = (async () => {
+			const settings = {
+				headers: {
+					Accept: 'application/json'
+				}
+			}
+			const res = await fetch('https://icanhazdadjoke.com', settings)
+			const data = await res.json()
+			return setDadJoke(data.joke)
+		})().catch(err=>console.log(err))
+	},[])
+	
+	// React.useEffect(() => {
+
+	// 	const fetchRandomFact = (async () => {
+	// 		const settings = {}
+	// 		const res = await fetch(`https://api.fungenerators.com/fact/onthisday/event`)
+	// 		const data = await res.json()
+	// 		return setDailyFact(data.contents.event)
+	// 	}) ()
+	// }, [] )
 
 	return(
 		<div>
@@ -45,7 +48,7 @@ React.useEffect(() => {	const fetchDadJoke = async () => {
 			<ul>
 				<li>JSON Placeholder: {jsonplaceholder}</li>
 				<li>Dad Joke: {dadJoke}</li>
-				<li>API.com: {apiCom}</li>
+				{/* <li>API.com: {dailyFact}</li> */}
 			</ul>
 		</div>
 	)
