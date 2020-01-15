1b4eb78c41d7df44dd31c47dd3f46b372d44e111
Brian Coutts <bjhcoutts@gmail.com>
2020-01-14 21:04:03 -0800

    global variable restructure/optimization

--- src/components/main-menu/main-menu.tsx
+++ src/components/main-menu/main-menu.tsx
@@ -5,16 +5,24 @@ export const MainMenu = () => {
 	return(
 		<Container>
 			<h2>This site is currently a lovingly WIP</h2>
-		<MenuItem>Ethereal</MenuItem>
-		<MenuItem>Tangible</MenuItem>
-		<MenuItem>Design</MenuItem>
+			<nav>
+				<NavList>
+					<MenuItem>Ethereal</MenuItem>
+					<MenuItem>Tangible</MenuItem>
+					<MenuItem>Design</MenuItem>
+				</NavList>
+			</nav>
 		</Container>
 	)
 }
 
 const Container = styled.div``
 
-const MenuItem = styled.h2`
-font-size:2rem;
-margin-bottom: 1rem;
+const MenuItem = styled.li`
+	font-size:2rem;
+	margin-bottom: 1rem;
+`
+
+const NavList = styled.ul`
+
 `
